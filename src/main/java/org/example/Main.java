98be21eb8ecec7dcd1039e45158743445ef050a6
AdityaS98 <adityasharmalko1998@gmail.com>
2023-06-04 15:23:33 +0530

    Added : A Programme for displaying welcome message

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the program corresponding to the JDBC oepration in Addressbook");
+
+    }
+}
